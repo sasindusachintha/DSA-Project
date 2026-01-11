e35bc9eb0bd6ed96a26789d27e7370296d5a6028
Sasindu Sachintha <sasindusachintha1234@gmail.com>
2026-01-11 20:32:12 +0530

    Updated MergeSort with improved logic

--- src/DSA/MergeSort.java
+++ src/DSA/MergeSort.java
@@ -1,48 +1,57 @@
-
 package DSA;
 
 public class MergeSort {
-       public static void mergeSort(int[] arr, int left, int right){
-           if( left >= right) return ;
-           
-           int mid = (left + right) / 2 ;
-           
-           mergeSort(arr, left, mid);
-           mergeSort(arr, mid+1, right);
-           
-           merge(arr, left, mid, right);
-       }
-    
-    public static void merge(int[] arr, int left, int mid, int right){
+
+    public static void merge(int[] arr, int left, int mid, int right) {
         int i = left;
-        int j = mid+1;
+        int j = mid + 1;
         int k = 0;
-        
-        int[] temp = new int[right-left+1];
-        
-        while(i <= mid && j <= right){
-            if(arr[i] <= arr[j]){
+
+        int[] temp = new int[right - left + 1];
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
                 temp[k++] = arr[i++];
-            }else{
+            } else {
                 temp[k++] = arr[j++];
             }
         }
-        while( i <= mid)   temp[k++] = arr[i++];
-        while( j <= right) temp[k++] = arr[j++];
-        
-        for(int x=0 ; x < temp.length; x++){
-            arr[left+x] = temp[x];
+        while (i <= mid) {
+            temp[k++] = arr[i++];
         }
-        
-    }
-    public static void main(String[] args){
-        int[] arr = {3,2,5,8};
-        
-        mergeSort(arr, 0, arr.length-1);
-        
-        for(int x: arr){
-            System.out.print(x + " ");
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+        for (int p = 0; p < temp.length; p++) {
+            arr[left + p] = temp[p];
         }
     }
 
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {56, 43, 32, 45};
+
+        System.out.println("Original Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 }
