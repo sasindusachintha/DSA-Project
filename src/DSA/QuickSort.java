163ee6b1044fd98335ad61f16c2cc70de9a46e2f
Sasindu Sachintha <sasindusachintha1234@gmail.com>
2026-01-12 15:57:53 +0530

    Improved QuickSort with Time and Space Complexity and Enhanced Code

--- src/DSA/QuickSort.java
+++ src/DSA/QuickSort.java
@@ -1,38 +1,53 @@
-
 package DSA;
 
-
 public class QuickSort {
-    public static void quicksort(int[] arr, int low, int high){
-        if( low < high ){
-            int pi = partition(arr, low, high);
-            
-            quicksort(arr, low, pi-1);
-            quicksort(arr, pi+1, high);
-        }
-    }
-    public static int partition(int[] arr, int low, int high){
-        int pivot = arr[high];
-        int i = low-1;
-        
-        for(int j= low ; j< high ; j++){
-            if(arr[j] <= pivot){
-                i++;   
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+
+    public static int partition(int[] arr, int left, int right) {
+        int pivot = arr[right];
+        int i = left - 1;
+
+        for (int j = left; j < right; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
-        int temp = arr[i+1];
-        arr [i+1]= arr[high];
-        arr[high] =  temp;
-        return i+1;
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[right];
+        arr[right] = temp;
+
+        return i + 1;
     }
-    public static void main(String[] args){
-        int[] arr = {8,3,5,2};
-        quicksort(arr, 0, arr.length-1);  
-        for(int x: arr){
-            System.out.print(x+" ");
+
+    public static void quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int index = partition(arr, left, right);
+
+            quickSort(arr, left, index - 1);
+            quickSort(arr, index + 1, right);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {87, 34, 21, 56, 34};
+
+        System.out.println("Original Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("Sorted Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
         }
     }
 }
+
+// Time Complexity :- Best/Avg - O(nlogn) Worst - O(n2)
+// Time Complexity :- O(logn)
+// Stable: No
